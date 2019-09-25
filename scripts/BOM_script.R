e38ca3bb49ad32a45feff7764896e4914cd6b665
Weihs <wei115@csiro.au>
2019-09-25 17:08:05 +1000

    Added solutions for question 1 and question 2

--- scripts/BOM_script.R
+++ scripts/BOM_script.R
@@ -8,18 +8,37 @@ BOM_stations <- read_csv("data/BOM_stations.csv")
 BOM_data 
 separated_min_max <- separate(BOM_data, col = Temp_min_max, into = c('min', 'max'), sep = "/") 
 
-only_min <- separated_min_max %>% filter(min != '-') %>% 
-  group_by(Station_number)
-nrow(only_min)
+only_data <- separated_min_max %>% 
+  filter(min != '-') %>% 
+  filter(max != '-') %>% 
+  filter(Rainfall != '0') %>% 
+  group_by(Station_number) %>% 
+  summarise(n = n())
+only_data
 
-only_max <- separated_min_max %>% filter(max != '-') %>% 
-  group_by(Station_number) %>% summary()
-only_max
+#Question 2 -
+#Which month saw the lowest average daily temperature difference?
 
-only_rainfall <- separated_min_max %>% filter(Rainfall != '-') %>% 
-  group_by(Station_number) %>% summary()         
-only_rainfall
-
-
+#Solution 1
 separated_min_max
+separated_min_max <- separate(BOM_data, col = Temp_min_max, into = c('min', 'max'), sep = "/") 
+avrg_temp_diff <- separated_min_max %>% 
+  mutate(temp_diff = as.numeric(max) - as.numeric(min)) %>% 
+  filter(temp_diff != 'NA') %>% 
+  group_by(Month) %>% 
+  summarise(avrg_temp_diff = mean(temp_diff)) %>% 
+  arrange(avrg_temp_diff)
+avrg_temp_diff
 
+#Solution 2
+avrg_temp_diff <- separated_min_max %>% 
+  mutate(temp_diff = as.numeric(max) - as.numeric(min)) %>% 
+  filter(min != '-') %>% 
+  filter(max != '-') %>% 
+  group_by(Month) %>% 
+  summarise(avrg_temp_diff = mean(temp_diff)) %>% 
+  arrange(avrg_temp_diff)
+avrg_temp_diff
+
+#Question 3 -
+#Which state saw the lowest average daily temperature difference?
